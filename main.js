f5dade01c1cebc3d53cfdc9597e2de32a6462745
Matias Arias <matiasarias59@gmail.com>
2022-09-09 19:46:29 -0300

    def func ingresar cliente

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const ingresarDatoCliente = (dato) => {
     }
     return input;  
 }   
-
 //Definicion Clase Cliente
 class Client{
     constructor(nomb, dni, tel, mail, dir){
@@ -20,4 +19,14 @@ class Client{
     mostrar() {
         return ("Nombre: "+this.nomb+"\nDNI: "+this.dni+"\nTelefono: "+this.tel+"\nE-mail: "+this.mail+"\nDireccion: "+this.dir);
     }   
+}
+//Funcion ingresar Cliente
+const ingresarCliente = () =>{
+    const cliente = new Client(
+        ingresarDatoCliente("Nombre"),
+        ingresarDatoCliente("DNI"),
+        ingresarDatoCliente("Telefono"),
+        ingresarDatoCliente("E-mail"),
+        ingresarDatoCliente("Domicilio"));
+    return cliente;
 }
